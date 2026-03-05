9fefb6999a3238ab950cc9480299aa1da4e36d79
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-03-05 19:20:55 +0530

    sum of running arrays

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,39 +1,19 @@
 #include <iostream>
+#include<vector>
 using namespace std;
-
-void insertionSort(int arr[], int n)
+int main() {
+int n ;
+cin>>n;
+vector<int>a(n);
+for(int j = 0 ; j<n ;j++)
 {
-    for(int i = 1; i < n; i++)
-    {
-        int cur = arr[i];
-        int prev = i - 1;
-        while(prev >= 0 && arr[prev] > cur)
-        {
-            arr[prev + 1] = arr[prev];
-            prev--;
-        }
-        arr[prev + 1] = cur;
-    }
+    cin>>a[j];
 }
-
-int main()
+int sum=0 ;
+for(int i =0 ; i<n; i++)
 {
-    int n;
-    cin >> n;
-
-    int arr[n];
-
-    for(int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    insertionSort(arr, n);
-
-    for(int i = 0; i < n; i++)
-    {
-        cout << arr[i] <<" ";
-    }
-
-    return 0;
+    sum+=a[i];
+    cout<<sum<<" ";
+}
+return 0 ;
 }
