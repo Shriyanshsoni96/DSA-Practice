4cb6a3fac0b69c91e5a056cc5a924bc01a53574b
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-01 23:36:01 +0530

    buiilf in functions in string

---
+++ Leetcode/Build_in_Functions/string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+// string s="123";
+//   int n = stoi(s);
+//    cout<<n;
+// uses for converting number of string into a integer
+
+return 0;
+}
