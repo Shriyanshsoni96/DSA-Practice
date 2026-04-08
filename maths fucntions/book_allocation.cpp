c5226ad216faedb992d6a271b23ed0014b589078
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-08 17:08:36 +0530

    book Allocation problem

--- maths fucntions/book_allocation.cpp
+++ maths fucntions/book_allocation.cpp
@@ -1,65 +1,16 @@
 #include<iostream>
 #include<vector>
+#include<numeric>
+#include<algorithm>
 using namespace std;
 
-bool isvalid(vector<int> &arr , int n , int m , int maximum )
-{
-    int student = 1 , pages = 0 ; 
-    for(int i = 0 ; i<n ; i++ )
+int main()
+{   int m = 2; 
+    vector<int> arr ={10,20,30,40};
+    int s = *max_element(arr.begin(),arr.end());
+    int e =  accumulate(arr.begin(), arr.end(),0);
+    while(s<e)
     {
-        if(arr[i]>maximum)
-        {
-            return false; 
-        }
-        if(pages+arr[i]<=maximum)
-        {
-            pages+=arr[i];
-        }
-        else
-        {
-            student++;
-            pages=arr[i];
-        }
+        
     }
-    return student > m ? false : true;
-}
-
-int allocation(vector<int> &arr , int n , int m )
-{   if(m>n)
-    {
-        return -1;
-    }
-    int sum = 0;
-    for(int i = 0 ;i<n;i++ )
-    {
-        sum+=arr[i];
-    }
-
-    int ans = -1 ;  
-    int start = 0 , end = sum ; 
-
-   while(start<=end)
-   {
-    int mid = start+(end-start)/2;
-    
-    if(isvalid(arr,n,m,mid))//mid shift to left 
-    { 
-            ans = mid; 
-            end=mid-1;
-    }
-    else// mid shift to right  
-    {
-        start = mid+1;
-    }
-   } 
-//    cout<<sum<<endl;
-return ans;
-}
-
-int main(){
-vector<int> arr ={12,34,67,90};
-int n = 40;// no of pages 
-int m =2 ; //no of students
-cout<<allocation(arr,n,m)<<endl;
-return 0;
 }
