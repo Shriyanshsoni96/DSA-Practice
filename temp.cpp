abb5be981ea1e38c29c2612612b7bfe07dc47e0e
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-21 19:30:29 +0530

    pratice book alocation

--- temp.cpp
+++ temp.cpp
@@ -1,9 +1,31 @@
-#include <iostream>
+#include<iostream>
+#include<numeric>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
-int main() {
-    while (true) {
-        cout << "Infinite Loop Running..." << endl;
+int main(){
+vector<int> arr={10,20,30,40};
+int k = 2; 
+
+int sum = accumulate(arr.begin(),arr.end(),0);
+int right= sum ;
+int left = *max(arr.begin(),arr.end());
+int ans = 0 ;
+while(left<right)
+{
+    int mid= left+ (right-left)/2;
+    if(ispossible(k , arr, mid))
+    {
+        ans= mid;
+        right = mid-1;
     }
-    return 0;
+    else
+    {
+        
+        left = mid+1; 
+    }
+}
+
+return 0;
 }
