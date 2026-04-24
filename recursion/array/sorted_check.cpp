e8733d4c980690ef41788e1a3f4c0a7d72678c5b
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-24 14:40:09 +0530

    checking for the sorted array

--- recursion/array/sorted_check.cpp
+++ recursion/array/sorted_check.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+bool isSort(int num[], int size){
+
+    if(size == 1 or size == 0){      // base case
+        return true;
+    }
+
+    if(num[0] < num[1] and isSort(num + 1, size - 1)){ // recursive function
+        return true;
+    }
+
+    return false;
+}
+
 int main(){
+int num[] = {1,2,3,4,0};
+cout<< boolalpha << isSort(num, 5);
 
 return 0;
 }
+
+//correct code
