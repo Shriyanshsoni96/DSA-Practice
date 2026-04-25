c2b40a886f52772d52b31c727bdfc4bd34b3f8a6
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-25 20:51:29 +0530

    dalily practse \

--- Z Last Folder/temp.cpp
+++ Z Last Folder/temp.cpp
@@ -1,54 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int sumDigits(int n)
-{
-    if(n == 0) return 0;
-    return (n % 10) + sumDigits(n / 10);
+int furthestDistance(string moves) {
+    int L = 0, R = 0, blank = 0;
+
+    for(char ch : moves) {
+        if(ch == 'L') L++;
+        else if(ch == 'R') R++;
+        else blank++;
+    }
+
+    int right = R + blank - L;
+    int left = L + blank - R;
+
+    return max(abs(right), abs(left));
 }
 
-int main()
-{
-    int n = 546;
-    cout << sumDigits(n);
-    return 0;
-}
-
-#include<iostream>
-using namespace std;
-
-bool isPalindrome(string s, int start, int end)
-{
-    if(start >= end) return true;
-    if(s[start] != s[end]) return false;
-    return isPalindrome(s, start + 1, end - 1);
-}
-
-int main()
-{
-    string s = "NITIN";
-    if(isPalindrome(s, 0, s.length() - 1))
-        cout << "Palindrome";
-    else
-        cout << "Not Palindrome";
-    return 0;
-}
-
-#include<iostream>
-using namespace std;
-
-int findMax(int arr[], int n)
-{
-    if(n == 1) return arr[0];
-    int maxFromRest = findMax(arr, n - 1);    
-    return max(arr[n - 1], maxFromRest);
-}
-
-int main()
-{
-    int arr[] = {3, 7, 2, 9, 5};
-    int n = 5;
-
-    cout << findMax(arr, n);
+int main() {
+    string moves = "L_RL__R";
+    cout << furthestDistance(moves);
     return 0;
 }
