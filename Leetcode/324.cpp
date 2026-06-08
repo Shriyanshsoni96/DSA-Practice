c247c52841b00d2c34ea85990a1fe50e42f76f7d
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-06-08 07:07:40 +0530

    bruteforce but test case 1 pass only

--- Leetcode/324.cpp
+++ Leetcode/324.cpp
@@ -1,12 +1,37 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 int main(){
-    vector<int>nums = {1,5,1,1,6,4};
+    vector<int>nums = {1,3,2,2,3,1};
 vector<int> temp = nums ; 
-for(auto i : temp)
+
+sort(temp.begin(), temp.end());
+int left= 0 ;
+int right = temp.size()-1;
+for(int i = 0 ; i<temp.size();i++)
 {
-    cout<<i<<endl;
+    if(i%2==0)//even condition he 
+    {
+        if(temp[left]<temp[right])
+        {
+            nums[i]=temp[left];
+            left++;
+        }
+    }
+    else
+    {
+        if(temp[right]>temp[left])
+        {
+            nums[i]=temp[right];
+            right--;
+        }
+    }
+}
+
+for(auto i : nums)
+{
+    cout<<i<<",";
 }
 return 0;
 }
