978712c2abfaba245918f962d40fd0a4ec352fcf
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-06-24 20:56:55 +0530

    Declaration of Hashing

---
+++ Hashing/declaration.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n =5;
+    int arr[5]={1,2,1,4,3};
+    int hash[13]={0};
+
+    for(int i =0 ; i < n ; i++)
+    {
+        hash[arr[i]]+=1;
+    }
+
+    int q;
+    cin>>q;
+    while(q--)
+    {   int number;
+        cin>>number;
+        cout<<hash[number]<<endl;
+        
+    }
+}
