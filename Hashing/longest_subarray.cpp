7aef4a9a6b6424717d375323841b1e765f757f03
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-06-29 19:35:33 +0530

    longeest subarray

---
+++ Hashing/longest_subarray.cpp
@@ -0,0 +1,21 @@
+#include<map>
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    int a[] ={1,2,3,1,1,1,1};
+    int n = 7;
+    int k = 3;
+    map<long long ,int> presum;
+    
+    long long sum= 0 ;
+    int maxlen=0;
+    for(int i = 0 ; i<n;i++)
+    {
+        sum+=a[i];
+        
+    }
+
+
+} 
