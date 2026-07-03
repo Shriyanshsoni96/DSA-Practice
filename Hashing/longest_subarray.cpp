1cd9aef06b28e8ba689a700b93d6c092394f2711
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-07-03 20:01:48 +0530

    longest subarray with the prefix sum and hsahmap

--- Hashing/longest_subarray.cpp
+++ Hashing/longest_subarray.cpp
@@ -1,21 +1,38 @@
+// Longest subarray with the sum k of consicutive subarray 
 #include<map>
 #include<iostream>
 #include<climits>
+#include<vector>
 using namespace std;
-int main()
+int longestsubarray(vector<int> &a , int &k )
 {
-    int a[] ={1,2,3,1,1,1,1};
-    int n = 7;
-    int k = 3;
     map<long long ,int> presum;
-    
     long long sum= 0 ;
     int maxlen=0;
-    for(int i = 0 ; i<n;i++)
+    for(int i = 0 ; i<a.size();i++)
     {
         sum+=a[i];
-        
+        if(sum==k)
+        {
+            maxlen=max(maxlen,i+1);
+        }
+        int rem=sum-k;
+        if(presum.find(rem) != presum.end())
+        {
+            int len = i-presum[rem];
+            maxlen=max(maxlen,len);
+        }
+        if(presum.find(sum) == presum.end())
+        {
+            presum[sum]=i;
+        }
     }
+    return maxlen;
+}
 
-
+int main()
+{
+    vector<int> a ={1,2,3,1,1,1,1};
+    int k = 3;
+    cout<<longestsubarray(a,k);
 } 
