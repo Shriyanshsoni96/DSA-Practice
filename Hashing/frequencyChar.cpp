a2e37afbeacc26a7326a70cb78799dd79959196e
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-07-08 23:19:59 +0530

    char

--- Hashing/frequencyChar.cpp
+++ Hashing/frequencyChar.cpp
@@ -4,21 +4,21 @@
 using namespace std;
 int main(){
 // this code is for the direct access of all the elements in the charcters 
-// vector<char> arr = {'A','b','c','E','d','a','A','E',};
-// 
-//     int hash[256]={0};
-//     for(int i =0 ; i < arr.size() ; i++)
-//     {
-//         hash[arr[i]]++;
-//     }
+vector<char> arr = {'A','b','c','E','d','a','A','E',};
+
+    int hash[256]={0};
+    for(int i =0 ; i < arr.size() ; i++)
+    {
+        hash[arr[i]]++;
+    }
     
-//     int q;
-//     cin>>q;
-//     while(q--)
-//     {   char number;
-//         cin>>number;
-//         cout<<hash[number]<<endl;
-//     }
+    int q;
+    cin>>q;
+    while(q--)
+    {   char number;
+        cin>>number;
+        cout<<hash[number]<<endl;
+    }
 
 // this code is for the direct access of only lowercase the elements in the charcters 
 
@@ -41,19 +41,19 @@ int main(){
 
 // this code is for the direct access of only Uppercase of  the elements in the charcters 
 
-vector<char> arr = {'A','C','E','A','C'};
-    int hash[26]={0};
-    for(int i =0 ; i < arr.size() ; i++)
-    {
-        hash[arr[i]-'A']++;
-    }
+// vector<char> arr = {'A','C','E','A','C'};
+//     int hash[26]={0};
+//     for(int i =0 ; i < arr.size() ; i++)
+//     {
+//         hash[arr[i]-'A']++;
+//     }
     
-    int q;
-    cin>>q;
-    while(q--)
-    {   char number;
-        cin>>number;
-        cout<<hash[number-'A']<<endl;
-    }
+//     int q;
+//     cin>>q;
+//     while(q--)
+//     {   char number;
+//         cin>>number;
+//         cout<<hash[number-'A']<<endl;
+//     }
 return 0;
 }
