1045733a294b731810ac694df7fd06eb97cd9f32
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-08-03 22:41:07 +0530

    402 leetcode

---
+++ Leetcode/402.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<string> nums ={"4","3","2","2","1"","9"};
+int prev=nums[0];
+int del = 0 ; 
+for(int  i =1 ; i < n ; i++)
+{
+      if(prev>i )
+{   s.remove(i-1,i);
+del++;}
+else{
+prev=s[i];
+}
+
+}
+return s ;
+return 0;
+}
